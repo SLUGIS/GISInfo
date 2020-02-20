a53d84b5f369153e44631a9c83fd2b7507e09de3
hunter-waite <hwaite@calpoly.edu>
2020-02-20 13:28:55 -0800

    Updated refresh time to 5 minutes instead of 1

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 
 function init(){
-  window.setInterval("reloadIFrame();", 60000);
+  window.setInterval("reloadIFrame();", 300000); // interval at 300000 mS ( 5 s)
 }
 
+// grabs the iframes for the google sheets and the calendars
+// sets their sources to themselves thus refreshing them without refreshing
+// the page itself
 function reloadIFrame() {
   document.getElementById("Sheets").src = document.getElementById("Sheets").src;
   document.getElementById("Calendar").src = document.getElementById("Calendar").src;
